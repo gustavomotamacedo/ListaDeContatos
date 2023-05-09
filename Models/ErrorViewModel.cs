5f8832e184ecacae848dc1a23838bf47bc8cc2a8
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2023-05-09 18:58:18 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ListaContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
